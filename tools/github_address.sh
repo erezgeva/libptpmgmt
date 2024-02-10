933468970293c4cd73b3be16aa21e88dfde47f14
Erez Geva <ErezGeva2@gmail.com>
2024-02-10 01:26:31 +0100

    Fix github_address.sh dispatcher.
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- tools/github_address.sh
+++ tools/github_address.sh
@@ -344,9 +344,9 @@ EOF
 ###############################################################################
 main()
 {
+ local -r me1="$(realpath -s "$0")"
  cd "$(dirname "$(realpath "$0")")/.."
  source tools/util.sh
- local -r me1="$(realpath -s "$0")"
  local -r dr1="$(dirname "$me1")"
  local -r me="${me1//$dr1\//}"
  local n
