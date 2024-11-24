522abccc75f79a90f6c437a9e4b1ea0bd8fbd8b5
Erez Geva <ErezGeva2@gmail.com>
2024-11-25 00:32:22 +0100

    clkmgr: Improve ClientState unit test.
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- clkmgr/utest/client_state.cpp
+++ clkmgr/utest/client_state.cpp
@@ -9,54 +9,125 @@
  *
  */
 
-#define NSEC_PER_SEC    (1000000000)
-
 #include "clockmanager.h"
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
 using namespace clkmgr;
 
-TEST(ClientStateTest, ApiTest)
+// ClientState(ClientState &newState);
+TEST(ClientStateTest, copy_constructor)
 {
-    ClientState cstate = {};
-    struct timespec current_time = {};
-    TransportClientId refClientID = { 0x41, 0x42, 0x43 };
-    Event_state eState = {};
-    Event_count eCount = {};
+    ClientState cstate1;
+    cstate1.set_connected(true);
+    ClientState cstate2(cstate1);
+    EXPECT_TRUE(cstate2.get_connected());
+}
+
+// void set_clientState(ClientState &newState);
+TEST(ClientStateTest, set_clientState)
+{
+    ClientState cstate1;
+    cstate1.set_connected(true);
+    ClientState cstate2;
+    EXPECT_FALSE(cstate2.get_connected());
+    cstate2.set_clientState(cstate1);
+    EXPECT_TRUE(cstate2.get_connected());
+}
+
+// bool get_connected() const;
+// void set_connected(bool state);
+TEST(ClientStateTest, connected)
+{
+    ClientState cstate;
     cstate.set_connected(true);
-    EXPECT_EQ(cstate.get_connected(), true);
+    EXPECT_TRUE(cstate.get_connected());
+}
+
+// bool get_subscribed() const;
+// void set_subscribed(bool subscriptionState);
+TEST(ClientStateTest, subscribed)
+{
+    ClientState cstate;
     cstate.set_subscribed(true);
-    EXPECT_EQ(cstate.get_subscribed(), true);
-    cstate.set_sessionId(0x3f);
-    EXPECT_EQ(cstate.get_sessionId(), 0x3f);
-    cstate.set_ptp4l_id(0x55);
-    EXPECT_EQ(cstate.get_ptp4l_id(), 0x55);
+    EXPECT_TRUE(cstate.get_subscribed());
+}
+
+// TransportClientId get_clientID();
+// void set_clientID(const TransportClientId &cID);
+TEST(ClientStateTest, clientID)
+{
+    ClientState cstate;
+    TransportClientId refClientID = { 0x41, 0x42, 0x43 };
     cstate.set_clientID(refClientID);
-    if(clock_gettime(CLOCK_REALTIME, &current_time) == -1)
-        printf("clock_gettime function failure\n");
-    else {
-        cstate.set_last_notification_time(current_time);
-        EXPECT_EQ(cstate.get_last_notification_time().tv_sec, \
-            current_time.tv_sec);
-        EXPECT_EQ(cstate.get_last_notification_time().tv_nsec, \
-            current_time.tv_nsec);
-    }
-    eState.notification_timestamp = current_time.tv_sec;
-    eState.notification_timestamp *= NSEC_PER_SEC;
-    eState.notification_timestamp += current_time.tv_nsec;
+    //EXPECT_EQ(cstate.get_clientID(), { 0x41, 0x42, 0x43} );  // TODO
+}
+
+// Event_count &get_eventStateCount();
+// void set_eventStateCount(const Event_count &eCount);
+TEST(ClientStateTest, eventStateCount)
+{
+    ClientState cstate;
+    Event_count eCount = {};
+    eCount.as_capable_event_count = 0x2;
+    eCount.offset_in_range_event_count = 0x4;
+    cstate.set_eventStateCount(eCount);
+    Event_count &e = cstate.get_eventStateCount();
+    EXPECT_EQ(e.as_capable_event_count, 0x2);
+    EXPECT_EQ(e.offset_in_range_event_count, 0x4);
+}
+
+// Event_state &get_eventState();
+// void set_eventState(const Event_state &eState);
+TEST(ClientStateTest, eventState)
+{
+    ClientState cstate;
+    Event_state eState = {};
+    eState.notification_timestamp = 141000268448;
     eState.as_capable = true;
     eState.offset_in_range = true;
     eState.synced_to_primary_clock = true;
     eState.composite_event = false;
     eState.gm_changed = false;
     eState.clock_offset = 0x1234;
-    eCount.as_capable_event_count = 0x2;
-    eCount.offset_in_range_event_count = 0x4;
     cstate.set_eventState(eState);
-    cstate.set_eventStateCount(eCount);
-    EXPECT_EQ(cstate.get_eventStateCount().as_capable_event_count, 0x2);
-    EXPECT_EQ(cstate.get_eventStateCount().offset_in_range_event_count, 0x4);
-    EXPECT_EQ(cstate.get_eventState().as_capable, true);
-    EXPECT_EQ(cstate.get_eventState().composite_event, false);
+    Event_state &e = cstate.get_eventState();
+    EXPECT_EQ(e.notification_timestamp, 141000268448);
+    EXPECT_EQ(e.clock_offset, 0x1234);
+    EXPECT_TRUE(e.offset_in_range);
+    EXPECT_TRUE(e.synced_to_primary_clock);
+    EXPECT_TRUE(e.as_capable);
+    EXPECT_FALSE(e.gm_changed);
+    EXPECT_FALSE(e.composite_event);
+}
+
+// void set_last_notification_time(const struct timespec &last_notification_time);
+// struct timespec get_last_notification_time();
+TEST(ClientStateTest, notification_time)
+{
+    ClientState cstate;
+    cstate.set_last_notification_time({14, 5019});
+    struct timespec c = cstate.get_last_notification_time();
+    EXPECT_EQ(c.tv_sec, 14);
+    EXPECT_EQ(c.tv_nsec, 5019);
+}
+
+// std::string toString(); // TODO
+
+// ClkMgrSubscription &get_eventSub(); // TODO
+
+// sessionId_t get_sessionId() const;
+// void set_sessionId(sessionId_t sessionId);
+TEST(ClientStateTest, sessionId)
+{
+    ClientState cstate;
+    cstate.set_sessionId(0x3f);
+    EXPECT_EQ(cstate.get_sessionId(), 0x3f);
+}
+
+// void set_ptp4l_id(uint8_t ptp4l_id);
+// uint8_t get_ptp4l_id() const;
+TEST(ClientStateTest, ptp4l_id)
+{
+    ClientState cstate;
+    cstate.set_ptp4l_id(0x55);
+    EXPECT_EQ(cstate.get_ptp4l_id(), 0x55);
 }
