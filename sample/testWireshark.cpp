4b7b006115d0f35c192d11e5063a3d8b242ec032
Erez Geva <ErezGeva2@gmail.com>
2025-05-04 19:07:29 +0200

    Sample Test Wireshark: add signalling packets
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- sample/testWireshark.cpp
+++ sample/testWireshark.cpp
@@ -18,22 +18,20 @@
 #include <ptpmgmt/proc.h>
 #include <ptpmgmt/sock.h>
 #include <ptpmgmt/err.h>
+#include <ptpmgmt/sig.h>
 
 using namespace ptpmgmt;
 
 SockIp4 sk;
 Message msg;
+size_t curLen;
 uint8_t buf[400];
 uint8_t bufBase[100];
-const Binary ip("\x12\x34\x56\x78", 4);
-const Binary physicalAddress = Binary("\xc4\x7d\x46\x20\xac\xae", 6);
-const ClockIdentity_t clockId = { 196, 125, 70, 255, 254, 32, 172, 174 };
-const PortAddress_t portAddress0 = { IEEE_802_3, 6, physicalAddress };
-const PortAddress_t portAddress1 = { UDP_IPv4, 4, ip };
-const PortIdentity_t a0 = { clockId, 1 };
-const PortIdentity_t a1 = { { 9, 8, 7, 6, 5, 4, 1, 7}, 2 };
-const PortIdentity_t a2 = { { 19, 28, 77, 6, 15, 24, 21, 7}, 7 };
-
+const PortAddress_t portAddress0 = { IEEE_802_3, 6,
+                        Binary("\xc4\x7d\x46\x20\xac\xae", 6)
+                    };
+const PortAddress_t portAddress1 = { UDP_IPv4, 4, Binary("\x12\x34\x56\x78", 4) };
+/* ********************************************************************* */
 /* Create PTPv2 packet using a TLV object
  * For TLVs that support set */
 void buidSend(mng_vals_e id, const BaseMngTlv *dataSend)
@@ -161,11 +159,11 @@ void UNICAST_MASTER_TABLE_f()
 void ACCEPTABLE_MASTER_TABLE_f()
 {
     ACCEPTABLE_MASTER_TABLE_t t;
-    t.list.push_back({a0, 127});
-    t.list.push_back({a1, 111});
+    t.list.push_back({{ { 196, 125, 70, 255, 254, 32, 172, 174 }, 1 }, 127});
+    t.list.push_back({{ { 9, 8, 7, 6, 5, 4, 1, 7}, 2 }, 111});
     buidSend(ACCEPTABLE_MASTER_TABLE, &t);
     /* The second one have a pad */
-    t.list.push_back({a2, 65});
+    t.list.push_back({{ { 19, 28, 77, 6, 15, 24, 21, 7}, 7 }, 65});
     buidSend(ACCEPTABLE_MASTER_TABLE, &t);
 }
 void ACCEPTABLE_MASTER_TABLE_ENABLED_f()
@@ -265,6 +263,11 @@ bool rspReady()
     *ptr16 = cpu_to_net16(MANAGEMENT);
     return true;
 }
+/* A base PTPv2 packet we add a TLV on top */
+void base()
+{
+    memcpy(buf, bufBase, 50);
+}
 void rsp(uint16_t mngId, uint8_t *tlv, size_t tlvLength)
 {
     size_t len = 54 + tlvLength;
@@ -277,11 +280,6 @@ void rsp(uint16_t mngId, uint8_t *tlv, size_t tlvLength)
     memcpy(ptr16, tlv, tlvLength);           // 54
     if(!sk.send(buf, len))
         printf("rsp %s\n", Error::getError().c_str());
-}
-/* A base PTPv2 packet we add a TLV on top */
-void base()
-{
-    memcpy(buf, bufBase, 50);
 }
 /* ********************************************************************* */
 void CLOCK_DESCRIPTION_f()
@@ -394,6 +392,193 @@ void TRANSPARENT_CLOCK_DEFAULT_DATA_SET_f()
     rsp(0x4000, m, sizeof m);
 }
 /* ********************************************************************* */
+/* Library support parsing of signaling, but not their creation
+ * Therefor we create the signaling packets with TLVs data
+ */
+bool sigReady()
+{
+    // signaling = 36 header + 10 targetPortIdentity = 44
+    if(!msg.setAction(GET, NULL_PTP_MANAGEMENT) ||
+        msg.build(bufBase, sizeof bufBase, 1) != MNG_PARSE_ERROR_OK) {
+        printf("bufBase %s\n", Error::getError().c_str());
+        return false;
+    }
+    bufBase[0] = (bufBase[0] & 0xf0) | Signaling; // messageType
+    bufBase[32] = 5; // controlField
+    return true;
+}
+void baseSig()
+{
+    curLen = 44;
+    memcpy(buf, bufBase, 44);
+}
+void addTlv(tlvType_e type, uint8_t *tlv, size_t len)
+{
+    uint16_t *ptr16 = (uint16_t *)(buf + curLen);
+    *ptr16++ = cpu_to_net16(type);
+    *ptr16++ = cpu_to_net16(len);
+    memcpy(ptr16, tlv, len);
+    curLen += len + 4;
+}
+void addMngTlv(uint16_t mngId, uint8_t *tlv, size_t len)
+{
+    uint16_t *ptr16 = (uint16_t *)(buf + curLen);
+    *ptr16++ = cpu_to_net16(MANAGEMENT);
+    *ptr16++ = cpu_to_net16(len + 2);
+    *ptr16++ = cpu_to_net16(mngId);
+    memcpy(ptr16, tlv, len);
+    curLen += len + 6;
+}
+void sendSig()
+{
+    // header.messageLength
+    uint16_t *ptr16 = (uint16_t *)(buf + 2);
+    *ptr16 = cpu_to_net16(curLen);
+    if(!sk.send(buf, curLen))
+        printf("sendSig %s\n", Error::getError().c_str());
+}
+/* ********************************************************************* */
+void OneManagementTlvs_f()
+{
+    baseSig();
+    uint8_t m[2] = {137}; // PRIORITY1 priority1 = 137
+    addMngTlv(0x2005, m, sizeof m);
+    sendSig();
+}
+void TwoManagementTlvs_f()
+{
+    baseSig();
+    uint8_t m1[2] = {119}; // PRIORITY2 priority2 = 119
+    uint8_t m2[2] = {7};  // DOMAIN domainNumber = 7
+    addMngTlv(0x2006, m1, sizeof m1);
+    addMngTlv(0x2007, m2, sizeof m2);
+    sendSig();
+}
+void OrgTwoManagementTlvs_f()
+{
+    baseSig();
+    uint8_t m1[2] = {119}; // PRIORITY2 priority2 = 119
+    uint8_t m2[10] = {0x12, 0x34, 0x56, 7, 0x19, 0xa3, 0x17, 0x23, 0x75, 0x31};
+    uint8_t m3[2] = {7};  // DOMAIN domainNumber = 7
+    addMngTlv(0x2006, m1, sizeof m1);
+    addTlv(ORGANIZATION_EXTENSION_PROPAGATE, m2, sizeof m2);
+    addMngTlv(0x2007, m3, sizeof m3);
+    sendSig();
+}
+void LoopTwoManagementTlvs_f()
+{
+    baseSig();
+    uint8_t m1[2] = {137}; // PRIORITY1 priority1 = 137
+    uint8_t m2[2] = {119}; // PRIORITY2 priority2 = 119
+    addMngTlv(0x2005, m1, sizeof m1);
+    addMngTlv(0x2006, m2, sizeof m2);
+    sendSig();
+}
+void AllOrgTlvs_f()
+{
+    baseSig();
+    uint8_t m1[10] = {0x12, 0x34, 0x15, 7, 0xf3, 0xa3, 0x17, 3, 5, 1};
+    uint8_t m2[10] = {0x23, 0x13, 0x27, 8, 7, 0xb2, 9, 7, 0xb5, 3};
+    uint8_t m3[10] = {0x45, 2, 0x29, 9, 0x19, 0xc5, 3, 9, 0xd7, 5};
+    addTlv(ORGANIZATION_EXTENSION, m1, sizeof m1);
+    addTlv(ORGANIZATION_EXTENSION_PROPAGATE, m2, sizeof m2);
+    addTlv(ORGANIZATION_EXTENSION_DO_NOT_PROPAGATE, m3, sizeof m3);
+    sendSig();
+}
+void FilterWithOrgTlvs_f()
+{
+    baseSig();
+    uint8_t m1[10] = {0x12, 0x34, 0x15, 7, 0xf3, 0xa3, 0x17, 3, 5, 1};
+    uint8_t m2[10] = {0x23, 0x13, 0x27, 8, 7, 0xb2, 9, 7, 0xb5, 3};
+    uint8_t m3[10] = {0x45, 2, 0x29, 9, 0x19, 0xc5, 3, 9, 0xd7, 5};
+    addTlv(ORGANIZATION_EXTENSION, m1, sizeof m1);
+    addTlv(ORGANIZATION_EXTENSION_PROPAGATE, m2, sizeof m2);
+    addTlv(ORGANIZATION_EXTENSION_DO_NOT_PROPAGATE, m3, sizeof m3);
+    sendSig();
+}
+//  layer 1 synchronization, port communication availability,
+//  protocol address and cumulative rate ratio TLVs
+void MngErrMoreTlvs_f()
+{
+    baseSig();
+    // PRIORITY1, WRONG_LENGTH, "test"
+    uint8_t m0[18] = {0, 3, 0x20, 5, 0, 0, 0, 0, 4, 116, 101, 115, 116};
+    uint8_t m1[22] = {4, 0x80, 0, 0x76, 0x5c, 0xbb, 0xcb, 0xe3, 0xd4, 0x12,
+            0x57, 0x89, 0x19, 0x33, 0x24, 5, 97, 108, 116, 101, 114
+        };
+    uint8_t m2[] = {15, 7};
+    uint8_t m3[] = {15, 15};
+    uint8_t m4[8] = {0, 1, 0, 4, 0x12, 0x34, 0x56, 0x78};
+    uint8_t m5[] = {0x99, 0x1a, 0x11, 0xbd};
+    addTlv(MANAGEMENT_ERROR_STATUS, m0, sizeof m0);
+    addTlv(ALTERNATE_TIME_OFFSET_INDICATOR, m1, sizeof m1);
+    addTlv(L1_SYNC, m2, sizeof m2);
+    addTlv(PORT_COMMUNICATION_AVAILABILITY, m3, sizeof m3);
+    addTlv(PROTOCOL_ADDRESS, m4, sizeof m4);
+    addTlv(CUMULATIVE_RATE_RATIO, m5, sizeof m5);
+    sendSig();
+}
+void VectorTlvs_f()
+{
+    baseSig();
+    uint8_t m0[16] = {196, 125, 70, 255, 254, 32, 172, 174, 5, 7, 9, 1,
+            172, 201, 3, 45
+        };
+    addTlv(PATH_TRACE, m0, sizeof m0);
+    uint8_t m1[78] = {196, 125, 70, 255, 254, 32, 172, 174, 0, 1,
+            4, 0, 0, 0x90, 8, 0x20, 0x11, 0, 0x36, 0xf9, 0xdf, 0xb8,
+            0x45, 0x38, 0xaf, 0xb7, 0x17, 0x94, 0xd2, 0xa1, 0x99, 0x1a, 0x11,
+            0xbd, 0, 0x98, 0x41, 0, 2, 0x4e, 0x38, 0xd0, 0, 0,
+            11, 0xc7, 0, 0x81, 4, 8, 0x22, 8, 0, 0, 0, 0,
+            0x12, 0x43, 0x5b, 0x4a, 0xf4, 0xd4, 0x1e, 0x48, 0xbd, 0xde,
+            0xfa, 0x5c, 0, 0x81, 0x90, 0x58, 0x24, 0x20, 0x38, 0x1a, 0, 0
+        };
+    addTlv(SLAVE_RX_SYNC_TIMING_DATA, m1, sizeof m1);
+    uint8_t m2[56] = {196, 125, 70, 255, 254, 32, 172, 174, 0, 1, 7, 0,
+            11, 0xe6, 0x81, 1, 0x14, 0, 2, 0x24, 4, 0, 0x81, 0x12, 0x14, 0, 2,
+            0x20, 4, 0, 0xbe, 0xbd, 0xe0, 0,
+            7, 3, 0x81, 0x12, 0x14, 0x50, 0xb0, 0x20, 4, 0, 0x98, 0x42, 0x14,
+            0x50, 0xb0, 0x20, 4, 0, 0xbe, 0x95, 0x4e, 0xf0
+        };
+    addTlv(SLAVE_RX_SYNC_COMPUTED_DATA, m2, sizeof m2);
+    uint8_t m3[36] = {196, 125, 70, 255, 254, 32, 172, 174, 0, 1, 9, 0,
+            2, 0xf1, 0, 2, 9, 8, 2, 0x20,
+            0x36, 0x61, 0x20, 0x10,
+            9, 0xf3, 0, 0x20, 0, 0x90, 8, 0x40,
+            0x36, 0x61, 0x6d, 0x7c
+        };
+    addTlv(SLAVE_TX_EVENT_TIMESTAMPS, m3, sizeof m3);
+    sendSig();
+}
+void AccuracyTlv_f()
+{
+    baseSig();
+    uint8_t m[88] = {171, 231, 0, 0, 0x80, 0x2c, 0x40, 0, 0x48, 0x10, 5, 0,
+            64, 234, 32, 6, 102, 102, 102, 102, 0x91, 0x3c, 0x40, 0, 0x4e,
+            0x80, 5, 0, 65, 0, 143, 2, 184, 81, 235, 133, 0x91, 0x60, 0, 0,
+            0x4e, 0xB5, 5, 0, 65, 210, 102, 120, 12, 99, 231, 109, 0x91,
+            0x60, 0, 0, 0x4e, 0x90, 0x90, 0x65, 65, 149, 197, 130, 9, 172,
+            244, 31, 0x91, 0x6a, 0x54, 4, 0x4e, 0x94, 0x90, 0x65, 65, 199,
+            136, 144, 30, 119, 223, 59
+        };
+    addTlv(ENHANCED_ACCURACY_METRICS, m, sizeof m);
+    sendSig();
+}
+/* ********************************************************************* */
+/* This packet is create from data only
+ */
+void SMPTE_Org()
+{
+    uint8_t m[100] = {0xd, 2, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0x74, 0xda, 0x38, 0xff, 0xfe, 0xf6, 0x98, 0x5e, 0, 1, 0, 0, 4,
+            0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 3,
+            3, 3, 0, 0, 3, 0, 0x30, 0x68, 0x97, 0xe8, 0, 0, 1, 0, 0, 0, 0x1e, 0,
+            0, 0, 1, 1
+        };
+    if(!sk.send(m, sizeof m))
+        printf("SMPTE %s\n", Error::getError().c_str());
+}
+/* ********************************************************************* */
 int main()
 {
     if(!sk.setUdpTtl(1) || !sk.setIfUsingName("lo") || !sk.init()) {
@@ -450,5 +635,21 @@ int main()
     ALTERNATE_TIME_OFFSET_MAX_KEY_f();
     TRANSPARENT_CLOCK_PORT_DATA_SET_f();
     TRANSPARENT_CLOCK_DEFAULT_DATA_SET_f();
+    // Create signaling packets
+    // TLVs data is verifyed in the library unit tests
+    if(!sigReady())
+        return 0;
+    OneManagementTlvs_f();
+    TwoManagementTlvs_f();
+    OrgTwoManagementTlvs_f();
+    LoopTwoManagementTlvs_f();
+    AllOrgTlvs_f();
+    FilterWithOrgTlvs_f();
+    MngErrMoreTlvs_f();
+    VectorTlvs_f();
+    AccuracyTlv_f();
+    // SMPTE
+    // Packet data is verifyed in the library unit tests
+    SMPTE_Org();
     return 0;
 }
