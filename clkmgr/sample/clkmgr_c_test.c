intel-staging/libptpmgmt_iaclocklib

clkmgr: Add support for retrieve logSyncInterval from ptp4l.
This patch introduces functionality to retrieve the `logSyncInterval` from the
ptp4l daemon and display it in the clkmgr system. The changes include:

    - Remove `PORT_PROPERTIES_NP`.
    - Modified the `PORT_DATA_SET` callback to handle different event.
    - `logSyncInterval` is retrieved from ptp4l daemon and stored in the `event`
      structure.
    - Introduce reset_PortDataSet() to consolidate all the action to reset
      the related port data.
    - Modified the test application to display the `gm_logSyncInterval`

These changes improve the visibility of the Grandmaster's `logSyncInterval`
so that the behavior is transparent and controllable in the clkmgr system.

Signed-off-by: Goh, Wei Sheng <wei.sheng.goh@intel.com>

clkmgr/sample/clkmgr_c_test.c
@@ -277,6 +277,8 @@ int main(int argc, char *argv[])
                 "clock_offset", event_state.clock_offset);
         printf("| %-25s | %-19ld ns |\n",
                 "notification_timestamp", event_state.notification_timestamp);
+        printf("| %-25s | %-19ld us |\n",
+                "gm_logSyncInterval", event_state.ptp4l_sync_interval);
         printf("+---------------------------+------------------------+\n");
         if (subscription.composite_event_mask) {
             printf("| %-25s | %-22d |\n", "composite_event",
@@ -369,6 +371,8 @@ int main(int argc, char *argv[])
                 "clock_offset", event_state.clock_offset);
             printf("| %-25s |     %-19ld ns |\n",
                 "notification_timestamp", event_state.notification_timestamp);
+            printf("| %-25s |     %-19ld us |\n",
+                "gm_logSyncInterval", event_state.ptp4l_sync_interval);
             printf("+---------------------------+--------------+-------------+\n");
             if (subscription.composite_event_mask) {
                 printf("| %-25s | %-12d | %-11d |\n", "composite_event",

clkmgr/sample/clkmgr_test.cpp
@@ -285,6 +285,8 @@ int main(int argc, char *argv[])
                 "clock_offset", eventState.clock_offset);
         printf("| %-25s | %-19ld ns |\n",
                 "notification_timestamp", eventState.notification_timestamp);
+        printf("| %-25s | %-19ld us |\n",
+                "gm_logSyncInterval", eventState.ptp4l_sync_interval);
         printf("+---------------------------+------------------------+\n");
         if (composite_event) {
             printf("| %-25s | %-22d |\n", "composite_event",
@@ -377,6 +379,8 @@ int main(int argc, char *argv[])
                 "clock_offset", eventState.clock_offset);
             printf("| %-25s |     %-19ld ns |\n",
                 "notification_timestamp", eventState.notification_timestamp);
+            printf("| %-25s |     %-19ld us |\n",
+                "gm_logSyncInterval", eventState.ptp4l_sync_interval);
             printf("+---------------------------+--------------+-------------+\n");
             if (composite_event) {
                 printf("| %-25s | %-12d | %-11d |\n", "composite_event",

erezgeva/libptpmgmt

    - Remove and consolidate `portDataSet()` into `PORT_DATA_SET` callback.
    - Introduce `reset_PortDataSet()` to consolidate all the action to reset
    - Modified the test application to display the `gm_sync_interval`.

clkmgr/sample/clkmgr_c_test.c
@@ -277,6 +277,8 @@ int main(int argc, char *argv[])
                 "clock_offset", event_state.clock_offset);
         printf("| %-25s | %-19ld ns |\n",
                 "notification_timestamp", event_state.notification_timestamp);
+        printf("| %-25s | %-19ld us |\n",
+                "gm_sync_interval", event_state.ptp4l_sync_interval);
         printf("+---------------------------+------------------------+\n");
         if (subscription.composite_event_mask) {
             printf("| %-25s | %-22d |\n", "composite_event",
@@ -369,6 +371,8 @@ int main(int argc, char *argv[])
                 "clock_offset", event_state.clock_offset);
             printf("| %-25s |     %-19ld ns |\n",
                 "notification_timestamp", event_state.notification_timestamp);
+            printf("| %-25s |     %-19ld us |\n",
+                "gm_sync_interval", event_state.ptp4l_sync_interval);
             printf("+---------------------------+--------------+-------------+\n");
             if (subscription.composite_event_mask) {
                 printf("| %-25s | %-12d | %-11d |\n", "composite_event",

clkmgr/sample/clkmgr_test.cpp
@@ -285,6 +285,8 @@ int main(int argc, char *argv[])
                 "clock_offset", eventState.clock_offset);
         printf("| %-25s | %-19ld ns |\n",
                 "notification_timestamp", eventState.notification_timestamp);
+        printf("| %-25s | %-19ld us |\n",
+                "gm_sync_interval", eventState.ptp4l_sync_interval);
         printf("+---------------------------+------------------------+\n");
         if (composite_event) {
             printf("| %-25s | %-22d |\n", "composite_event",
@@ -377,6 +379,8 @@ int main(int argc, char *argv[])
                 "clock_offset", eventState.clock_offset);
             printf("| %-25s |     %-19ld ns |\n",
                 "notification_timestamp", eventState.notification_timestamp);
+            printf("| %-25s |     %-19ld us |\n",
+                "gm_sync_interval", eventState.ptp4l_sync_interval);
             printf("+---------------------------+--------------+-------------+\n");
             if (composite_event) {
                 printf("| %-25s | %-12d | %-11d |\n", "composite_event",