407ac7fb630689dbfea99b0733c402836432897f
Erez Geva <ErezGeva2@gmail.com>
2025-05-10 16:34:11 +0200

    clkmgr: improve Signal handling module
    Replace macros with constant and inline function.
    Mark local functions as static and inline.
    
    Put global function inside a name space declaration.
    
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- clkmgr/common/sighandler.cpp
+++ clkmgr/common/sighandler.cpp
@@ -14,69 +14,62 @@
 
 #include <signal.h>
 
-#define INTR_SIGNAL (SIGUSR1)
+static const int INTR_SIGNAL = SIGUSR1;
 
 __CLKMGR_NAMESPACE_USE;
 
 using namespace std;
 
-#define SIGADDSET(set,sig,ret)            \
-    if(sigaddset(set, sig) == -1)         \
-        return ret;
-
-bool AddStopSignal(sigset_t *sigset)
+static inline bool SIGADDSET(sigset_t *sigset, int sig)
 {
-    SIGADDSET(sigset, SIGINT,  false);
-    SIGADDSET(sigset, SIGHUP,  false);
-    SIGADDSET(sigset, SIGTERM, false);
-    SIGADDSET(sigset, SIGQUIT, false);
-    SIGADDSET(sigset, SIGALRM, false);
-    SIGADDSET(sigset, SIGABRT, false);
-    return true;
+    return sigaddset(sigset, sig) == 0;
 }
 
-bool AddInterruptSignal(sigset_t *sigset)
+static inline bool AddStopSignal(sigset_t *sigset)
 {
-    SIGADDSET(sigset, SIGUSR1, false);
-    SIGADDSET(sigset, SIGUSR2, false);
-    return true;
+    return SIGADDSET(sigset, SIGINT) &&
+        SIGADDSET(sigset, SIGHUP) &&
+        SIGADDSET(sigset, SIGTERM) &&
+        SIGADDSET(sigset, SIGQUIT) &&
+        SIGADDSET(sigset, SIGALRM) &&
+        SIGADDSET(sigset, SIGABRT);
 }
 
-bool GenerateBlockSigset(sigset_t *block)
+static inline bool AddInterruptSignal(sigset_t *sigset)
 {
-    sigemptyset(block);
-    if(!AddStopSignal(block))
-        return false;
-    if(!AddInterruptSignal(block))
-        return false;
-    return true;
+    return SIGADDSET(sigset, SIGUSR1) &&
+        SIGADDSET(sigset, SIGUSR2);
 }
 
-bool GenerateWaitSigset(sigset_t *wait)
+static inline bool GenerateBlockSigset(sigset_t *block)
 {
-    sigemptyset(wait);
-    if(!AddStopSignal(wait))
-        return false;
-    return true;
+    return sigemptyset(block) == 0 &&
+        AddStopSignal(block) &&
+        AddInterruptSignal(block);
 }
 
-bool clkmgr::BlockStopSignal()
+static inline bool GenerateWaitSigset(sigset_t *wait)
+{
+    return sigemptyset(wait) == 0 &&
+        AddStopSignal(wait);
+}
+
+__CLKMGR_NAMESPACE_BEGIN;
+
+bool BlockStopSignal()
 {
     sigset_t blockSigset;
-    if(!GenerateBlockSigset(&blockSigset))
-        return false;
-    if(pthread_sigmask(SIG_BLOCK, &blockSigset, nullptr) != 0)
-        return false;
-    return true;
+    return GenerateBlockSigset(&blockSigset) &&
+        pthread_sigmask(SIG_BLOCK, &blockSigset, nullptr) == 0;
 }
 
-bool clkmgr::WaitForStopSignal()
+bool WaitForStopSignal()
 {
     sigset_t waitSigset;
-    int cause;
     if(!GenerateWaitSigset(&waitSigset))
         return false;
     PrintDebug("Waiting for Interrupt Signal");
+    int cause;
     if(sigwait(&waitSigset, &cause) == -1) {
         PrintErrorCode("Waiting for Interrupt Signal");
         return false;
@@ -85,30 +78,33 @@ bool clkmgr::WaitForStopSignal()
     return true;
 }
 
-void NullSigaction(int sig, siginfo_t *siginfo, void *ctx) {}
-
-bool clkmgr::EnableSyscallInterruptSignal()
+// Empty callback for signalling
+static void NullSigaction(int sig, siginfo_t *siginfo, void *ctx)
 {
-    sigset_t unblockSigset;
-    struct sigaction intrSigaction;
-    intrSigaction.sa_sigaction = NullSigaction;
-    sigemptyset(&intrSigaction.sa_mask);
-    intrSigaction.sa_flags = SA_SIGINFO;
-    if(sigaction(INTR_SIGNAL, &intrSigaction, nullptr) == -1)
-        return false;
-    sigemptyset(&unblockSigset);
-    SIGADDSET(&unblockSigset, INTR_SIGNAL, false);
-    if(pthread_sigmask(SIG_UNBLOCK, &unblockSigset, nullptr) != 0)
-        return false;
-    return true;
 }
 
-bool clkmgr::SendSyscallInterruptSignal(thread &t)
+bool EnableSyscallInterruptSignal()
 {
-    int ret;
-    if((ret = pthread_kill(t.native_handle(), INTR_SIGNAL)) != 0) {
+    struct sigaction intrSigaction;
+    intrSigaction.sa_sigaction = NullSigaction;
+    if(sigemptyset(&intrSigaction.sa_mask) != 0)
+        return false;
+    intrSigaction.sa_flags = SA_SIGINFO;
+    sigset_t unblockSigset;
+    return sigaction(INTR_SIGNAL, &intrSigaction, nullptr) == 0 &&
+        sigemptyset(&unblockSigset) == 0 &&
+        SIGADDSET(&unblockSigset, INTR_SIGNAL) &&
+        pthread_sigmask(SIG_UNBLOCK, &unblockSigset, nullptr) == 0;
+}
+
+bool SendSyscallInterruptSignal(thread &t)
+{
+    int ret = pthread_kill(t.native_handle(), INTR_SIGNAL);
+    if(ret != 0) {
         PrintError("pthread_kill()", ret);
         return false;
     }
     return true;
 }
+
+__CLKMGR_NAMESPACE_END;
