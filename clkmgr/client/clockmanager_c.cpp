f788384768af18985c884e7a99fb501fa6d478e1
Goh, Wei Sheng <wei.sheng.goh@intel.com>
2025-06-06 17:43:59 +0800

    clkmgr: Refactor C wrapper for Clock Manager APIs
    This patch refactor the C wrapper for Clock Manager APIs to able
    to use the latest C++ APIs.
    
    Signed-off-by: Goh, Wei Sheng <wei.sheng.goh@intel.com>

--- clkmgr/client/clockmanager_c.cpp
+++ clkmgr/client/clockmanager_c.cpp
@@ -9,9 +9,10 @@
  *
  */
 
-#include "pub/clkmgr/clockmanager_c.h"
-#include "pub/clkmgr/timebase_configs.h"
 #include "pub/clockmanager.h"
+#include "pub/clkmgr/clockmanager_c.h"
+#include "pub/clkmgr/timebase_configs_c.h"
+#include "client/opaque_struct_c.hpp"
 
 #include <cstring>
 
@@ -26,111 +27,60 @@ bool clkmgr_disconnect()
     return ClockManager::disconnect();
 }
 
-size_t clkmgr_get_timebase_cfgs_size()
+size_t clkmgr_getTimebaseCfgsSize()
 {
     return TimeBaseConfigurations::size();
 }
 
-bool clkmgr_subscribe_by_name(const clkmgr_c_subscription sub,
-    const char *timeBaseName, Clkmgr_Event_state *cur_stat)
+bool clkmgr_subscribeByName(const Clkmgr_Subscription *sub_c,
+    const char *timeBaseName, Clkmgr_ClockSyncData *data_c)
 {
-    if(cur_stat == nullptr)
+    if(!data_c)
         return false;
     size_t timeBaseIndex = 0;
     if(TimeBaseConfigurations::BaseNameToBaseIndex(timeBaseName, timeBaseIndex))
-        return clkmgr_subscribe(sub, timeBaseIndex, cur_stat);
+        return clkmgr_subscribe(sub_c, timeBaseIndex, data_c);
     return false;
 }
 
-bool clkmgr_subscribe(const clkmgr_c_subscription sub, size_t time_base_index,
-    Clkmgr_Event_state *cur_stat)
+bool clkmgr_subscribe(const Clkmgr_Subscription *sub_c,
+    size_t timeBaseIndex, Clkmgr_ClockSyncData *data_c)
 {
-    if(cur_stat == nullptr || time_base_index == 0)
+    if(!sub_c || timeBaseIndex == 0 || !data_c)
         return false;
-    return true;
-    // ToDo: Refactor C-wrapper
-    //ClockSubscriptionBasenewsub = {};
-    //Event_state state = {};
-    //bool ret;
-    /*    newsub.setEventMask(sub.event_mask);
-        newsub.define_threshold(thresholdGMOffset,
-            sub.threshold[Clkmgr_thresholdGMOffset].upper_limit,
-            sub.threshold[Clkmgr_thresholdGMOffset].lower_limit);
-        newsub.define_threshold(thresholdChronyOffset,
-            sub.threshold[Clkmgr_thresholdChronyOffset].upper_limit,
-            sub.threshold[Clkmgr_thresholdChronyOffset].lower_limit);
-        newsub.set_composite_event_mask(sub.composite_event_mask);
-        ret = 0;//ClockManager::subscribe(newsub, time_base_index, state);
-        if(ret) {
-            cur_stat->as_capable = state.as_capable;
-            cur_stat->offset_in_range = state.offset_in_range;
-            cur_stat->synced_to_primary_clock = state.synced_to_primary_clock;
-            cur_stat->gm_changed = state.gm_changed;
-            cur_stat->composite_event = state.composite_event;
-            cur_stat->clock_offset = state.clock_offset;
-            cur_stat->notification_timestamp = state.notification_timestamp;
-            std::copy(std::begin(state.gm_identity), std::end(state.gm_identity),
-                std::begin(cur_stat->gm_identity));
-            cur_stat->ptp4l_sync_interval = state.ptp4l_sync_interval;
-            cur_stat->chrony_clock_offset = state.chrony_clock_offset;
-            cur_stat->chrony_reference_id = state.chrony_reference_id;
-            cur_stat->chrony_offset_in_range = state.chrony_offset_in_range;
-            cur_stat->polling_interval = state.polling_interval;
-        }
-     return ret;*/
+    // Create a C++ subscription object and set its parameters from the C struct
+    ClockSyncSubscription newSub;
+    if(clkmgr_isSubscriptionEnabled(sub_c, Clkmgr_ptpClock)) {
+        newSub.setPtpSubscription(*sub_c->ptp);
+        newSub.enablePtpSubscription();
+    }
+    if(clkmgr_isSubscriptionEnabled(sub_c, Clkmgr_sysClock)) {
+        newSub.setSysSubscription(*sub_c->sys);
+        newSub.enableSysSubscription();
+    }
+    return ClockManager::subscribe(newSub, timeBaseIndex, *data_c->data);
 }
 
-int clkmgr_status_wait_by_name(int timeout, const char *timeBaseName,
-    Clkmgr_Event_state *cur_stat, Clkmgr_Event_count *cur_cnt)
+int clkmgr_statusWaitByName(int timeout, const char *timeBaseName,
+    Clkmgr_ClockSyncData *data_c)
 {
-    if(cur_stat == nullptr)
+    if(!data_c)
         return -1;
     size_t timeBaseIndex = 0;
     if(TimeBaseConfigurations::BaseNameToBaseIndex(timeBaseName, timeBaseIndex))
-        return clkmgr_status_wait(timeout, timeBaseIndex, cur_stat, cur_cnt);
+        return clkmgr_statusWait(timeout, timeBaseIndex, data_c);
     return -1;
 }
 
-int clkmgr_status_wait(int timeout, size_t time_base_index,
-    Clkmgr_Event_state *cur_stat, Clkmgr_Event_count *cur_cnt)
+int clkmgr_statusWait(int timeout, size_t timeBaseIndex,
+    Clkmgr_ClockSyncData *data_c)
 {
-    if(cur_stat == nullptr || cur_cnt == nullptr || time_base_index == 0)
+    if(timeBaseIndex == 0 || !data_c)
         return -1;
-    Event_count eventCount = {};
-    Event_state state = {};
-    int ret = 0;
-    //int ret = ClockManager::status_wait(timeout, time_base_index, state,
-    //        eventCount);
-    //if(ret < 0)
-    //    return ret;
-    cur_stat->as_capable = state.as_capable;
-    cur_stat->offset_in_range = state.offset_in_range;
-    cur_stat->synced_to_primary_clock = state.synced_to_primary_clock;
-    cur_stat->gm_changed = state.gm_changed;
-    cur_stat->composite_event = state.composite_event;
-    cur_stat->clock_offset = state.clock_offset;
-    cur_stat->notification_timestamp = state.notification_timestamp;
-    std::copy(std::begin(state.gm_identity), std::end(state.gm_identity),
-        std::begin(cur_stat->gm_identity));
-    cur_stat->ptp4l_sync_interval = state.ptp4l_sync_interval;
-    cur_stat->chrony_clock_offset = state.chrony_clock_offset;
-    cur_stat->chrony_reference_id = state.chrony_reference_id;
-    cur_stat->chrony_offset_in_range = state.chrony_offset_in_range;
-    cur_stat->polling_interval = state.polling_interval;
-    if(ret > 0) {
-        cur_cnt->as_capable_event_count = eventCount.as_capable_event_count;
-        cur_cnt->composite_event_count = eventCount.composite_event_count;
-        cur_cnt->gm_changed_event_count = eventCount.gm_changed_event_count;
-        cur_cnt->offset_in_range_event_count =
-            eventCount.offset_in_range_event_count;
-        cur_cnt->synced_to_gm_event_count = eventCount.synced_to_gm_event_count;
-        cur_cnt->chrony_offset_in_range_event_count =
-            eventCount.chrony_offset_in_range_event_count;
-    }
-    return ret;
+    return ClockManager::statusWait(timeout, timeBaseIndex, *data_c->data);
 }
 
-bool clkmgr_gettime(timespec *ts)
+bool clkmgr_getTime(timespec *ts)
 {
     return ts != nullptr && clock_gettime(CLOCK_REALTIME, ts) == 0;
 }
--- clkmgr/pub/clkmgr/clockmanager_c.h
+++ clkmgr/pub/clkmgr/clockmanager_c.h
@@ -14,21 +14,13 @@
 
 #include "pub/clkmgr/types_c.h"
 #include "pub/clkmgr/timebase_configs_c.h"
+#include "pub/clkmgr/subscription_c.h"
+#include "pub/clkmgr/event_c.h"
 #include <time.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * structure to hold the event mask, composite event mask, and thresholds
- * for events that require user-defined threshold (upper and lower limits).
- */
-struct clkmgr_c_subscription {
-    uint32_t event_mask; /**< Events subscription mask */
-    uint32_t composite_event_mask; /**< Composite events mask */
-    /*struct Clkmgr_Threshold threshold[CLKMGR_THRESHOLD_MAX];*/ /**< Limits */
-};
 
 /**
  * Connect the client
@@ -46,65 +38,61 @@ bool clkmgr_disconnect();
  * Get the size of the time base configurations
  * @return The size of the time base configurations
  */
-size_t clkmgr_get_timebase_cfgs_size();
+size_t clkmgr_getTimebaseCfgsSize();
 
 /**
  * Subscribe to client events by name for the time base
- * @param[in] sub Subscription structure
+ * @param[in] sub_c Pointer to the Clkmgr_Subscription
  * @param[in] timeBaseName Name of the time base to be subscribed
- * @param[out] current_state Pointer to the current state structure
+ * @param[out] data_c Pointer to the Clkmgr_ClockSyncData
  * @return true on success, false on failure
  */
-bool clkmgr_subscribe_by_name(const struct clkmgr_c_subscription sub,
-    const char *timeBaseName, struct Clkmgr_Event_state *current_state);
+bool clkmgr_subscribeByName(const Clkmgr_Subscription *sub_c,
+    const char *timeBaseName, Clkmgr_ClockSyncData *data_c);
 
 /**
  * Subscribe to client events
- * @param[in] sub Subscription structure
- * @param[in] time_base_index Index of the time base to be subscribed
- * @param[out] current_state Pointer to the current state structure
+ * @param[in] sub_c Pointer to the Clkmgr_Subscription
+ * @param[in] timeBaseIndex Index of the time base to be subscribed
+ * @param[out] data_c Pointer to the Clkmgr_ClockSyncData
  * @return true on success, false on failure
  */
-bool clkmgr_subscribe(const struct clkmgr_c_subscription sub,
-    size_t time_base_index, struct Clkmgr_Event_state *current_state);
+bool clkmgr_subscribe(const Clkmgr_Subscription *sub_c,
+    size_t timeBaseIndex, Clkmgr_ClockSyncData *data_c);
 
 /**
  * Waits for a specified timeout period for any event changes by name of the
  * time base
- * @param[in] timeout TThe timeout in seconds. If timeout is 0, the function
+ * @param[in] timeout The timeout in seconds. If timeout is 0, the function
  * will check event changes once. If timeout is -1, the function will wait
  * until there is event changes occurs
  * @param[in] timeBaseName Name of the time base to be monitored
- * @param[out] current_state Pointer to the current state structure
- * @param[out] current_count Pointer to the event count structure
+ * @param[out] data_c Pointer to the Clkmgr_ClockSyncData
  * @return true if there is event changes within the timeout period,
  *         and false otherwise
  */
-int clkmgr_status_wait_by_name(int timeout, const char *timeBaseName,
-    struct Clkmgr_Event_state *current_state,
-    struct Clkmgr_Event_count *current_count);
+int clkmgr_statusWaitByName(int timeout, const char *timeBaseName,
+    Clkmgr_ClockSyncData *data_c);
 
 /**
  * Waits for a specified timeout period for any event changes
  * @param[in] timeout TThe timeout in seconds. If timeout is 0, the function
  * will check event changes once. If timeout is -1, the function will wait
  * until there is event changes occurs
- * @param[in] time_base_index Index of the time base to be monitored
- * @param[out] current_state Pointer to the current state structure
- * @param[out] current_count Pointer to the event count structure
+ * @param[in] timeBaseIndex Index of the time base to be monitored
+ * @param[out] data_c Pointer to the Clkmgr_ClockSyncData
  * @return true if there is event changes within the timeout period,
  *         and false otherwise
  */
-int clkmgr_status_wait(int timeout, size_t time_base_index,
-    struct Clkmgr_Event_state *current_state,
-    struct Clkmgr_Event_count *current_count);
+int clkmgr_statusWait(int timeout, size_t timeBaseIndex,
+    Clkmgr_ClockSyncData *data_c);
 
 /**
  * Retrieve the time of the CLOCK_REALTIME
  * @param[out] ts timestamp of the CLOCK_REALTIME
  * @return true on success
  */
-bool clkmgr_gettime(struct timespec *ts);
+bool clkmgr_getTime(struct timespec *ts);
 
 #ifdef __cplusplus
 }
