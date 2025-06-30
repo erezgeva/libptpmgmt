e917ff5e15cabe92815d8df10ea476eb1a23693b
Erez Geva <ErezGeva2@gmail.com>
2025-06-30 23:56:11 +0200

    clkmgr: run sample script
    The 'chrt' tool require the root permission.
    Skip it for normal user.
    
    Merge the 2 run scripts.
    
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- clkmgr/sample/run_clkmgr_c_test.sh
+++ clkmgr/sample/run_clkmgr_c_test.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-# SPDX-License-Identifier: BSD-3-Clause
-# SPDX-FileCopyrightText: Copyright © 2024 Intel Corporation.
-#
-# @author Christopher Hall <christopher.s.hall@@intel.com>
-# @copyright © 2024 Intel Corporation.
-#
-###############################################################################
-
-SCRIPT_PATH="$(dirname "$0")"
-TEST_PATH="${SCRIPT_PATH}/../../.libs/:/usr/local/lib/"
-
-LD_LIBRARY_PATH=$TEST_PATH chrt -f 99 $SCRIPT_PATH/clkmgr_c_test "$@"
+run_clkmgr_test.sh
--- clkmgr/sample/run_clkmgr_test.sh
+++ clkmgr/sample/run_clkmgr_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: Copyright © 2024 Intel Corporation.
 #
@@ -7,7 +7,16 @@
 #
 ###############################################################################
 
-SCRIPT_PATH="$(dirname "$0")"
-TEST_PATH="${SCRIPT_PATH}/../../.libs/:/usr/local/lib/"
-
-LD_LIBRARY_PATH=$TEST_PATH chrt -f 99 $SCRIPT_PATH/clkmgr_test "$@"
+main()
+{
+ local -r script_path="$(dirname "$(realpath "$0")")"
+ local -r script_name="$(basename "$0")"
+ local -r b="${script_name/run_/}"
+ local -r sample_name="${b/.sh/}"
+ local -r test_path="${script_path}/../../.libs/:/usr/local/lib/"
+ if  [[ $(id -u) -eq 0 ]] && command -v chrt > /dev/null; then
+  local -r use_chrt='chrt -f 99'
+ fi
+ LD_LIBRARY_PATH=$test_path $use_chrt $script_path/$sample_name "$@"
+}
+main "$@"
