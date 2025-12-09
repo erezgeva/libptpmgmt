e43713f882e5367e333f094815a1433544af383d
Erez Geva <ErezGeva2@gmail.com>
2025-12-09 21:11:39 +0100

    Fix C unit test
    Set value in structure before using.
    
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- uctest/msg.c
+++ uctest/msg.c
@@ -1633,6 +1633,7 @@ Test(MessageTest, MethodGetSendData)
     ptpmgmt_msg m = ptpmgmt_msg_alloc();
     cr_assert(not(zero(ptr, m)));
     struct ptpmgmt_PRIORITY1_t p;
+    p.priority1 = 1;
     cr_expect(m->setAction(m, PTPMGMT_SET, PTPMGMT_PRIORITY1, &p));
     cr_expect(eq(int, m->getBuildTlvId(m), PTPMGMT_PRIORITY1));
     cr_expect(eq(ptr, (void *)m->getSendData(m), &p));
