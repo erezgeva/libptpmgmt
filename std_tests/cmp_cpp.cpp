415d62b87103aea2c2ec325473956ee96d3bbe3b
Erez Geva <ErezGeva2@gmail.com>
2026-03-19 17:38:00 +0100

    Add body to 'std_tests/cmp_cpp.cpp' to actual libraries links.
    Signed-off-by: Erez Geva <ErezGeva2@gmail.com>

--- std_tests/cmp_cpp.cpp
+++ std_tests/cmp_cpp.cpp
@@ -9,4 +9,21 @@
 #include <ptpmgmt/timeCvrt.h>
 #include <ptpmgmt/ver.h>
 #include <clockmanager.h>
-int main(void){return 0;}
+int main(void)
+{
+    printf("compile version: %d.%d = %s (0x%d)\n", LIBPTPMGMT_VER_MAJ,
+        LIBPTPMGMT_VER_MIN, LIBPTPMGMT_VER, LIBPTPMGMT_VER_VAL);
+#ifdef __cplusplus
+    clkmgr::PTPClockSubscription ptp4lSub;
+    printf("running version: %d.%d = %s (0x%d)\n", ptpmgmt::getVersionMajor(),
+        ptpmgmt::getVersionMinor(), ptpmgmt::getVersion(),
+        ptpmgmt::getVersionValue());
+#else
+    Clkmgr_ClockSyncData *syncData = clkmgr_constructClockSyncDataInstance();
+    printf("running version: %d.%d = %s (0x%d)\n", ptpmgmt_getVersionMajor(),
+        ptpmgmt_getVersionMinor(), ptpmgmt_getVersion(), ptpmgmt_getVersionValue());
+    clkmgr_destroyClockSyncDataInstance(syncData);
+    printf("All events %d\n", CLKMGR_PTP_EVENT_ALL);
+#endif
+    return 0;
+}
